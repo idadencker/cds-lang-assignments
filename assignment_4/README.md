bed4f861492d255965c4b4e45ac81124ce6a5f84
idadencker <ida@dencker.net>
2024-05-14 21:39:57 +0200

    better plots and tqdm

--- assignment_4/README.md
+++ assignment_4/README.md
@@ -1,36 +1,50 @@
 # Emotion analysis with pre-trained language models 
 
+
 ## Introduction
-This program contains a script that will load a csv file of all lines from all 8 seasons of the poular televesion show Game of Thrones (GoT) to perform some computational text analysis. Using a pretrained sentiment analysis model, the emotion-english-distilroberta-base, an emotion for each line is extracted being either anger, disgust, fear, joy, neutral, sadness or surprise as well as a score ranging from 0-1 indication the intensity of the extracted emotion. 
-The model is based on the highly complex "DistilRoBERTa" architecture which is a distilled version of the RoBERTa model, which is itself a variant of the well-known BERT (Bidirectional Encoder Representations from Transformers) model, said to be a state-of-the-art model in natural langauge processing. 
-Plots demonstrating the distribution of all emotion labels in each season (8 seasons in total) and the relative frequency of each emotion across all seasons (7 emotions in total) is created and saved. The results are summarised and discussed.
+This program contains a script that will load a csv file of all lines from all 8 seasons of the popular television show Game of Thrones (GoT) to perform some computational text analysis. Using a pretrained sentiment analysis model, the emotion-english-distilroberta-base, an emotion for each line is extracted being either anger, disgust, fear, joy, neutral, sadness or surprise as well as a score ranging from 0-1 indicating the intensity of the extracted emotion. 
+The model is based on the highly complex "DistilRoBERTa" architecture which is a distilled version of the RoBERTa model, which is itself a variant of the well-known BERT (Bidirectional Encoder Representations from Transformers) model, said to be a state-of-the-art model in natural language processing. 
+Plots demonstrating the distribution of all emotion labels in each season (8 seasons in total) and the relative frequency of each emotion across all seasons (7 emotions in total) are created and saved. The results are summarised and discussed.
 
 
-## Data source
+## Data 
 The dataset is the ```Game of Thrones Script All Seasons``` dataset which consists of all spoken lines from the popular show Game of Thrones. The dataset has a total of 23911 lines. The dataset can be found and downloaded [here](https://www.kaggle.com/datasets/albenft/game-of-thrones-script-all-seasons?select%253DGame_of_Thrones_Script.csv). 
 
 
-## Repository structure
-The repository consists of 2 bash scripts, 1 README.md file, 1 txt file, and 3 folders. The folders contains the following:
-
--   in: for storing input data
--   out: holds the saved plots
--   src: script for doing emotion analysis on the GoT data
+## Repository overview 
+The repository consists of:
+- 1 README.md file
+- 2 bash scripts
+- 1 requirements file
+- in folder for storing input data
+- out folder for holding the saved results
+- src folder containing the script implementing emotion analysis 
 
 
 ## Reproducibility 
 To make the program work do the following:
 
-1) download the dataset and place it in the 'in' folder.
-2) in the script Emotion_analysis change the path to where the data is located 
-3) in a terminal start by running the following code (make sure your directory is set to where setup.sh is located):
-    $ source setup.sh
-4) in the terminal run the code:
-    $ source run.sh
-2 plots will be saved in the out folder. NOTE that the script will take some time to run since it deals with alot of data. 
+1) clone the repository 
+```python
+$ git clone https://github.com/idadencker/cds-lang-assignments.git
+```
+2) download the Game_of_Thrones_Script.csv and place in the 'in' folder
+3) In a terminal set your directory:
+```python
+$ cd assignment_4
+```
+4) To create a virtual environment run:
+```python
+$ source setup.sh
+```
+5) To run the script and save results 
+```python
+$ source run.sh 
+```
+2 plots are saved in the out folder
 
 
 ## Summary and discussion
 From looking at the first plot 'Count_all_emotions_for_seasons' it is clear that neutral is by far the most expressed emotion across all seasons. For all seasons anger is the second most expressed emotion. Generelly a very similar pattern for emotions is displayed for all 8 seasons, meaning that the distribution of emotions is close to identical for all seasons. 
-Looking at the relative frequencies of the different emotions across seasons, joy and disgust seems to decrease over the seasons, while more dispersed patterns are seen for the other emotions. Interestingly, the relative frequency of sadness notably drop for the last two seasons 7 and 8. Also fear and surprise is low in the last season campared to the ramaining seasons. Generelly, most emotions are quite stable for seasons with only a few example that deviance from the little-fluctuation-pattern.  
+Looking at the relative frequencies of the different emotions across seasons, joy and disgust seems to decrease over the seasons, while more dispersed patterns are seen for the other emotions. Interestingly, the relative frequency of sadness notably drop for the last two seasons 7 and 8. Also fear and surprise is low in the last season campared to the remaining seasons. Generelly, most emotions are quite stable for seasons with only a few examples that deviate from the little-fluctuation-pattern.  
 
--- assignment_4/src/Emotion_analysis.py
+++ assignment_4/src/Emotion_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tqdm import tqdm
 
 
 def extract_emotions(df):
@@ -12,73 +13,102 @@ def extract_emotions(df):
     classifier = pipeline("text-classification", 
                         model="j-hartmann/emotion-english-distilroberta-base", 
                         return_all_scores=False)
-
+    
     '''
     Text is made into list. Hereafter the text is classified using the pipeline and values are saved
     '''
     text = [str(i) for i in df['Sentence'].tolist()]
-    emotion_scores = classifier(text)
-
-    '''
-    Appends the items to the dataframe
-    '''
-    labels = []
-    scores = []
-
-    for item in emotion_scores:
-        labels.append(item['label'])
-        scores.append(item['score'])
-
+    
+    emotion_scores = []
+    for sentence in tqdm (text, desc = 'Extracting emotions on sentences'):
+        scores = classifier(sentence)
+        emotion_scores.append(scores[0])
+    
+    """Appends the items to the dataframe """
+    labels = [item['label'] for item in emotion_scores]
+    scores = [item['score'] for item in emotion_scores]
+    
     df['emotion'] = labels
     df['score'] = scores
 
     return df
 
 
-
-def emotions_count_for_seasons(df):
-    fig, axs = plt.subplots(3, 3, figsize=(15, 15))
-    '''
-    Loops through data and creates a histogram plot for each season
-    '''
-    for i, season_label in enumerate(df['Season'].unique()):
-            season_data = df[df['Season'] == season_label]
-            ax = season_data['emotion'].value_counts().sort_index().plot(kind='bar', title=f'Count of emotions in {season_label}', ax=axs[i // 3, i % 3])
-            ax.set_xlabel('Emotions')
-            ax.set_ylabel('Count')
-
-    plt.tight_layout()
-    plt.savefig("out/Count_all_emotions_for_seasons.png")
-    plt.show()
-
-
 def season_rel_freq_for_emotions(df):
-    fig, axs = plt.subplots(1, 6, figsize=(15, 15))
-    '''
-    Loops through data and creates a histogram plot for each emotion
-    '''
+    
+    """ Setting a style and generating a color palette for the bars """
+    sns.set_style("darkgrid")
+    num_seasons = len(df['Season'].unique())
+    color_palette = sns.color_palette("deep", num_seasons)
+
+    """ For arranging the plots, the number of unique emotions is calculated """
+    num_emotions = len(df['emotion'].unique())
+    
+    """ Calculates the number of rows and columns for subplot grid and creates the subplot grid """
+    num_rows = (num_emotions - 1) // 3 + 1
+    num_cols = min(num_emotions, 3)
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 15))
+    
+    """ Loops through data and creates a bar plot for each emotion """
     for i, emotion_label in enumerate(df['emotion'].unique()):
         emotion_data = df[df['emotion'] == emotion_label]
         counts = emotion_data['Season'].value_counts().sort_index()
         total_count = counts.sum()
-        relative_freq = counts / total_count  
-        ax = relative_freq.plot(kind='bar', title=f'Relative frequency of {emotion_label} across all seasons', ax=axs[i // 3, i % 3])
-        ax.set_xlabel('seasons')
+        relative_freq = counts / total_count 
+        ax = relative_freq.plot(kind='bar', color=color_palette , ax=axs[i // 3, i % 3])
+        ax.set_title(f'Relative frequency of {emotion_label} across all seasons')
+        ax.xaxis.set_label_text("")
         ax.set_ylabel('Relative Frequency')
-
+    
+    """ Hide the empty subplots for nicer layout """
+    for i in range(num_emotions, num_rows * num_cols):
+        axs[i // 3, i % 3].axis('off')
+    
     plt.tight_layout()
     plt.savefig("out/Relative_frequency_of_emotions_across_all_seasons.png")
     plt.show()
 
 
+def emotions_count_for_seasons(df):
+    
+    """ Setting a style and generating a color palette for the bars """
+    sns.set_style("darkgrid")
+    num_emotions = len(df['emotion'].unique())
+    color_palette = sns.color_palette("deep", num_emotions)
+
+    """ For arranging the plots, the number of unique emotions is calculated """
+    num_seasons = len(df['Season'].unique())
+    
+    """ Calculates the number of rows and columns for subplot grid and creates the subplot grid """
+    num_rows = (num_seasons - 1) // 3 + 1
+    num_cols = min(num_seasons, 3)
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 15))
+    
+    """ Loops through data and creates a bar plot for each season """
+    for i, season_label in enumerate(df['Season'].unique()):
+        season_data = df[df['Season'] == season_label]
+        ax = axs[i // 3, i % 3] 
+        emotions = season_data['emotion'].value_counts().sort_index()
+        ax.bar(emotions.index, emotions.values, color=color_palette)
+        ax.set_title(f'Count of emotions in {season_label}')
+        ax.xaxis.set_label_text("")
+        ax.set_ylabel('Count')
+    
+    """ Hide the empty subplots for nicer layout """
+    for i in range(num_seasons, num_rows * num_cols):
+        axs[i // 3, i % 3].axis('off')
+    
+    plt.tight_layout()
+    plt.savefig("out/Count_all_emotions_for_seasons.png")
+    plt.show()
+
 
 def main():
-    df = pd.read_csv("../../../../cds-lang-data/GoT-scripts/Game_of_Thrones_Script.csv")
+    df = pd.read_csv("in/Game_of_Thrones_Script.csv")
     df = extract_emotions(df)
-    emotions_count_for_seasons(df)
     season_rel_freq_for_emotions(df)
+    emotions_count_for_seasons(df)
 
     
-
 if __name__ == "__main__":
     main()
